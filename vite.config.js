bd46b6f8497c411a37036c3d06a8fecac999d654
Aleksandr Vladyko <mett0@bk.ru>
2023-06-08 12:12:35 +0700

    fixed

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+<<<<<<< HEAD
   base: "/blog_post_test/",
+=======
+  base: "/"
+>>>>>>> dc57982 (fixed)
 })
