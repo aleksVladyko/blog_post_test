64d7c75cb54a77c5b07e9140ce145bb64a8a7a0e
Aleksandr Vladyko <mett0@bk.ru>
2023-06-08 13:49:09 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 <<<<<<< HEAD
+<<<<<<< HEAD
   base: "/blog_post_test/",
 =======
   base: "/"
 >>>>>>> dc57982 (fixed)
+=======
+>>>>>>> fc7e75e (fix)
 })
